dca9ba06e9097465ae9bc7be00dbefef2dacb580
Eckart Bindewald <eckart.bindewald@precise-soft.com>
2024-04-14 09:36:12 -0400

    implemented single-chain folding

--- discomplex/pairfolding.py
+++ discomplex/pairfolding.py
@@ -7,12 +7,15 @@ from Bio.PDB import PDBParser, Selection, NeighborSearch
 import sys
 import io
 import os
+from os.path import dirname
 
 # Define PROJECT_HOME as two levels up from the current script
 PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 RAW_DIR = f'{PROJECT_HOME}/data/raw'
 INTERIM_DIR = f'{PROJECT_HOME}/data/interim'
 PROCESSED_DIR = f'{PROJECT_HOME}/data/processed'
+TABLE_DIR = f'{PROJECT_HOME}/tables/singlechain_folding'
+
 
 def load_fasta_sequences(fasta_file):
     """
@@ -35,6 +38,18 @@ def load_fasta_sequences(fasta_file):
             uniprot_id = record.description.split('|')[1]
             sequences[uniprot_id] = record
     return sequences
+
+def read_tabular(file):
+    if not os.path.exists(file):
+        raise FileNotFoundError(file)
+    df = None
+    if file.endswith("tsv"):
+        df = pd.read_csv(file, sep='\t')
+    elif file.endswith("csv"):
+        df = pd.read_csv(file)
+    else:
+        raise ValueError("Only formats CSV and TSV are supported.")
+    return df
 
 
 def protein_folding_predict_esm(seqs):
@@ -141,9 +156,9 @@ def find_interacting_residues(pdb_filename, cutoff=8.0,
     return regions
 
 
-def main(result_file = f'{INTERIM_DIR}/ppi_verified.tsv',
+def fold_pairs(result_file = f'{INTERIM_DIR}/ppi_verified.tsv',
         sequence_file = f'{RAW_DIR}/uniprot/uniprot_sprot.fasta.gz',
-        pdb_out_dir = f"{PROCESSED_DIR}/pdb_out",
+        pdb_out_dir = f"{PROCESSED_DIR}/pdb_pairs_out",
         binding_output_file=f'{PROCESSED_DIR}/binding_predictions_tmp.tsv',
         n=10,
         folding_method='esm',
@@ -200,6 +215,109 @@ def main(result_file = f'{INTERIM_DIR}/ppi_verified.tsv',
         os.makedirs(os.path.dirname(binding_output_file)) # create needed directory
     print("Writing predicted regions to file", binding_output_file)
     region_results.to_csv(binding_output_file, sep='\t')
+
+
+def fold_single_chain(ids,
+        ref_sequences = f'{RAW_DIR}/uniprot/uniprot_sprot.fasta.gz',
+        pdb_out_dir = f"{PROCESSED_DIR}/pdb_singles_out",
+        prediction_table_file=f"{TABLE_DIR}/disorder_predictions_folding.tsv",
+        n=10,
+        folding_method='esm', # esm or dmp
+        random_state=42):
+    """
+    Single-chain folding of a set of uniprot_ids
+    """
+    if not os.path.exists(dirname(prediction_table_file)):
+        print("Creating directory" , dirname(prediction_table_file))
+        os.makedirs(dirname(prediction_table_file))
+    if isinstance(ref_sequences, str):
+        ref_sequences = load_fasta_sequences(ref_sequences)
+    n = len(ids)
+    ids = list(ids)
+    binding_starts = []
+    binding_ends = []
+    ids1 = []
+    folded_paths=[]
+    for i in range(n):
+        id1 = ids[i]
+        # disprot = tbl.at[i,'disprot']
+        if id1 in ref_sequences:
+            seq1 = str(ref_sequences[id1].seq)
+            structure_shortname = f'{id1}'
+            # print(seq1)
+            # print(seq2)
+            # print()
+            seqs = [seq1]
+            folding_results = protein_folding_predict(seqs, method=folding_method)
+            pdb_output_file = pdb_out_dir + "/" + structure_shortname + f"_prediction_{folding_method}.pdb"    
+            if not os.path.exists(pdb_output_file):
+                if folding_results is not None and 'pdb_text' in folding_results and folding_results['pdb_text']: 
+                    folded_pdb = folding_results['pdb_text']
+                    with open(pdb_output_file, 'w') as f:
+                        print("Writing PDB structure to file", pdb_output_file)
+                        f.write(folded_pdb)
+                if os.path.exists(pdb_output_file):
+                    folded_paths.append(pdb_output_file)
+    #         if os.path.exists(pdb_output_file):
+    #             pdbdf = pdb_to_dataframe(pdb_output_file, count_base=0) # 1 for 1-based counting of output regions, 0 for 0-based output
+    #             for region in regions:
+    #                 binding_starts.append(region[0])
+    #                 binding_ends.append(region[1])
+    #                 ids1.append(id1)
+    #                 ids2.append(id2)
+    #                 region_results = pd.DataFrame(data={'id1':ids1,'id2':ids2, 'binding_start':binding_starts, 'binding_end':binding_ends})
+    #                 region_results.to_csv(binding_output_file + "_loop_tmp.tsv", sep='\t')
+    #         else:
+    #             print("Id pair", id1, id2, 'No PPi structure prediction obtained! not both in verified part of Uniprot/Swissprot!')
+
+    # dirname(prediction_table_file)
+    # region_results = pd.DataFrame(data={'id1':ids1,'id2':ids2, 'binding_start':binding_starts, 'binding_end':binding_ends})
+    # if not os.path.exists(os.path.dirname(binding_output_file)):
+    #     os.makedirs(os.path.dirname(binding_output_file)) # create needed directory
+    # print("Writing predicted regions to file", binding_output_file)
+    # region_results.to_csv(binding_output_file, sep='\t')
+    return {'folded_pdb_paths':folded_paths}
+
+def main(
+        instructions=f"{RAW_DIR}/disprot/disprot_2023-12.tsv",
+        id_col='acc',
+        mode = 'single',
+        ppi_file = f'{INTERIM_DIR}/ppi_verified.tsv',
+        ref_sequences = f'{RAW_DIR}/uniprot/uniprot_sprot.fasta.gz',
+        output_dir = f'{PROCESSED_DIR}/folding',
+        # output_table_file=None,
+        n=10,
+        folding_method='esm',
+        random_state=42):
+        
+    if isinstance(instructions, str):
+        instructions = read_tabular(instructions)
+    if not id_col in instructions.columns:
+        raise ValueError(f"Column {id_col} not found in instruction table")
+    if not os.path.exists(output_dir):
+        print("Creating output directory:", output_dir)
+        os.makedirs(output_dir)
+    if mode == 'single':
+        pdb_out_dir = f"{output_dir}/pdb_singlechain_out"
+        output_table_file =f'{output_dir}/disorder_predictions_tmp.tsv'
+    elif mode == 'pair':
+        pdb_out_dir = f"{output_dir}/pdb_pairs_out"
+        output_table_file = f'{output_dir}/binding_predictions_tmp.tsv'
+    ids = instructions[id_col].unique()
+    if not os.path.exists(pdb_out_dir):
+        print("Creating PDB output directory:", pdb_out_dir)
+        os.makedirs(pdb_out_dir)
+    if not isinstance(instructions, pd.DataFrame):
+        raise ValueError("information about what to fold has to be in tabular format.")
+    if mode == 'single':
+        result = fold_single_chain(ids,
+            ref_sequences = ref_sequences,
+            pdb_out_dir = pdb_out_dir,
+            prediction_table_file=output_table_file,
+            n=10,
+            folding_method='esm', # esm or dmp
+            random_state=42)
+    print("Paths folded PDB structures:", result)
 
 if __name__ == '__main__':
     main(n=10)
